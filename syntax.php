d3481221f18909929179c0dc9341ee6273f82dbe
Michael Klier chi@chimeric.de <chi@chimeric.de>
2007-01-15 18:04:54 +0100

    cosmetic changes
    darcs-hash:20070115170454-484ab-aa2d787907f33518efc32eecefbe072a75e458b3.gz

--- syntax.php
+++ syntax.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * plugin uparrow
+ * DokuWiki Syntax Plugin Uparrow
  * 
- * @license     GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author      Michael Klier <chi@chimeric.de>
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author  Michael Klier <chi@chimeric.de>
  */
 if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
@@ -25,8 +25,8 @@ class syntax_plugin_uparrow extends DokuWiki_Syntax_Plugin {
             'author' => 'Michael Klier',
             'email'  => 'chi@chimeric.de',
             'date'   => '2007-01-08',
-            'name'   => 'Plugin Uparrow',
-            'desc'   => 'Shows an uparrow which links to top of the page.',
+            'name'   => 'Uparrow',
+            'desc'   => 'Shows an arrow image which links to top of the page.',
             'url'    => 'http://www.chimeric.de/projects/dokuwiki/plugin/uparrow',
         );
     }
