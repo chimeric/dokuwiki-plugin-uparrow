604b43a67a8105ae82364d0d8e6be947c2b9f881
Michael Klier <chi@chimeric.de>
2007-05-24 23:18:06 +0200

    added gif support in configuration
    darcs-hash:20070524211806-23886-17ff3df68d95b2defb4bbfaabaabac502f2ee55a.gz

--- conf/metadata.php
+++ conf/metadata.php
@@ -3,5 +3,5 @@
  * configuraition metadata for uparrow plugin
  */
 
-$meta['image'] = array('string', '_pattern' => '#^[\-_a-zA-Z0-9]+\.(png|jpg|jpeg){1}$#');
+$meta['image'] = array('string', '_pattern' => '#^[\-_a-zA-Z0-9]+\.(png|jpg|jpeg|gif){1}$#');
 // vim:ts=4:sw=4:et:enc=utf-8:
