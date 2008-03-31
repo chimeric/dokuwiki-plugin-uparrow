ed9b8aed1dcf87df9f85b874b183816ada700490
Michael Klier <chi@chimeric.de>
2008-03-31 20:34:22 +0200

    added italian translation
    darcs-hash:20080331183422-23886-d90ac076534abdbf443c87c9b006377981c9f7fa.gz

---
+++ lang/it/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Italian language file for plugin uparrow settings
+ * @author Diego Pierotto <ita.translations@tiscali.it>
+ */
+
+$lang['image'] = 'Immagine freccia da usare';
+// vim:ts=4:sw=4:et:enc=utf-8:
