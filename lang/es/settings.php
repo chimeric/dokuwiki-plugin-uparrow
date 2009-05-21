03af84bf265d159a2f8208552500cf3ed91b46f3
Digna González Otero <digna.gonzalezotero@gmail.com>
2009-05-21 11:43:08 +0200

    added spanish translation

---
+++ lang/es/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * spanish language file for plugin uparrow settings
+ * @author     Digna González Otero <digna.gonzalezotero [at] gmail [dot] com>
+ */
+
+$lang['image'] = 'imagen de flecha a usar';
+// vim:ts=4:sw=4:et:enc=utf-8:
