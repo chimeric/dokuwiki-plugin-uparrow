3c17a51c7af388c96827fd8acd727820698fb828
Aleksandr Selivanov <alexgearbox@gmail.com>
2010-09-25 12:06:59 +0200

    added russian translation

---
+++ lang/ru/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Russian language file
+ * 
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+
+$lang['image'] = 'Используемое изображение стрелки (укажите название файла): tango-big.png, tango-medium.png, tango-small.png';
+$lang['auto']  = 'Автоматически вставлять изображение стрелки под каждым разделом';
+// vim:ts=4:sw=4:et:enc=utf-8:
